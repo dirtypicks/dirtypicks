7db1b299cc20e058133ef35e045bccb2fbf10e13
Edgar Corona <coronam.edgar@gmail.com>
2025-10-15 19:03:25 -0600

    Base front

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,16 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off"
+    },
+  }),
+]
+
 
 export default eslintConfig;
